67141f307a69255de44a9e46f51f07f963be813e
theo <theoswe@gmail.com>
2021-12-15 00:44:31 +0100

    weird bug

---
+++ day1/main.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> vals;
+
+int main ()
+{
+	// fetch input
+	ifstream valstream("input");
+	if (valstream.is_open())
+	{
+		string line;
+		while (getline(valstream, line))
+		{
+			int i = 0;
+			int value = stoi(line);
+			vals.push_back(value);
+		}
+	}
+
+	// part 1
+	int biggly = 0;
+	for (int i = 0; i < vals.size(); ++i)
+	{
+		if (vals[i] < vals[i+1])
+			biggly++;
+	}
+	//cout << biggly << endl;
+	cout << vals.size() << endl;
+
+	// part 2
+	int big;
+	for (int i = 0; i+2 < vals.size(); ++i)
+	{
+		int a = vals[i] + vals[i+1] + vals[i+2];
+		int b = vals[i+1] + vals[i+2] + vals[i+3];
+		//cout << a << ' ' << b << endl;
+		if (a < b)
+			big++;
+	}
+	cout << big << endl;
+	cout << vals.size() << endl;
+
+}
